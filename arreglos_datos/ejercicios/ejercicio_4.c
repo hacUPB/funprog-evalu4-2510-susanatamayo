93138bd6f112b218564ce22495612dccd9dedecf
Susana Tamayo <susana.tamayo@upb.edu.co>
2025-05-15 08:08:57 -0500

    Reto

--- arreglos_datos/ejercicios/ejercicio_4.c
+++
@@ -1,3 +0,0 @@
-// Ejercicio 4
-// - Leer 10 números desde teclado, almacenando los primeros 5 en un array y los últimos 5 en otro.
-// - Combinar ambos arrays en un tercer array e imprimir el contenido.
--- arreglos_datos/ejercicios/ejercicio_5.c
+++
@@ -1,7 +0,0 @@
-// Ejercicio 5
-//- Crear un arreglo unidimensional para almacenar notas de 24 estudiantes.
-//- Escribir una función para calcular:
-//- El promedio general del curso.
-//- La cantidad de estudiantes aprobados (nota ≥ 3).
-//- La cantidad de estudiantes reprobados (nota < 3).
-//- La cantidad de estudiantes con nota igual o superior a 4.
